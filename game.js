3a9a23fe1d154e3c6ec0388db75121f1d71961fe
Julia Cassidy <jac1007@wildcats.unh.edu>
2019-08-01 15:37:59 -0400

    adding java for buttons

--- game.js
+++ game.js
@@ -8,4 +8,16 @@ $(document).ready(function () {
 		for (i = 0; i < movies.length; i++) {
 			$("#movie-buttons").append("<button class='btn btn-success' data-movie='" + movies[i] + "'>" + movies[i] + "</button>");
 		}
-	}
+    }
+    
+    renderButtons();
+
+	// adding a button for movie entered in bar
+	$("#add-movie").on("click", function () {
+		event.preventDefault();
+		var movie = $("#movie-input").val().trim();
+		movies.push(movie);
+		renderButtons();
+		return;
+    });
+} )
