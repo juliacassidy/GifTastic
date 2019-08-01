ceb2bdba1f86eb1b14c551cb61890b9ba559d328
Julia Cassidy <jac1007@wildcats.unh.edu>
2019-08-01 15:36:02 -0400

    adding buttons for movies

--- game.js
+++ game.js
@@ -1,1 +1,11 @@
 
+$(document).ready(function () {
+	var movies = ["Hercules", "The Lion King", "The Dark Knight", "Jaws", "Star Wars", "The Great Gatsby", "Minions"];
+
+	// add buttons for movies i picked
+	function renderButtons() {
+		$("#movie-buttons").empty();
+		for (i = 0; i < movies.length; i++) {
+			$("#movie-buttons").append("<button class='btn btn-success' data-movie='" + movies[i] + "'>" + movies[i] + "</button>");
+		}
+	}
